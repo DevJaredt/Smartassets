8188e05dd3d2759b2272546351ce65726f47ab3d
Jared Trujillo <148577531+DevJaredt@users.noreply.github.com>
2024-11-21 21:59:56 -0500

    Feat: Create services

---
+++ src/app/modules/shared/services/toast/toast.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Toast } from '@capacitor/toast';
+import {
+  ToastDuration,
+  ToastPosition,
+} from 'src/app/modules/shared/types/Toast';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToastService {
+  constructor() {}
+  async showToast(
+    message: string,
+    duration: ToastDuration = 'short',
+    position: ToastPosition = 'bottom'
+  ) {
+    await Toast.show({
+      text: message,
+      duration: duration,
+      position: position,
+    });
+  }
+}
