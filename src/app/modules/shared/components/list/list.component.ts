ad2cdaed45f1c654bcbd73934d787ef1e9be8097
DevJaredt <jaredtrujilloespinosa64@gmail.com>
2024-11-22 16:22:57 -0500

    Feat: Add minor things

--- src/app/modules/shared/components/list/list.component.ts
+++ src/app/modules/shared/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IAsset } from '../../interfaces/IAssests';
 
 @Component({
   selector: 'app-list',
@@ -6,13 +7,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./list.component.scss'],
 })
 export class ListComponent {
+  @Input() assets!: IAsset[];
   @Input() label!: string;
   @Input() isClickable: boolean = false;
   @Output() itemClick = new EventEmitter<string>();
+  @Output() loanRequest: EventEmitter<IAsset> = new EventEmitter<IAsset>();
 
   protected onItemClick() {
     if (this.isClickable) {
       this.itemClick.emit(this.label);
     }
+  }
+
+  onLoanRequest(asset: IAsset) {
+    this.loanRequest.emit(asset);
   }
 }
