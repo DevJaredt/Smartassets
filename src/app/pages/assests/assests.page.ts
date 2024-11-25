6e7ab998f9ce43aa2f473f071c38f83435a3f03a
DevJaredt <jaredtrujilloespinosa64@gmail.com>
2024-11-25 16:27:39 -0500

    Refactor: change methods

--- src/app/pages/assests/assests.page.ts
+++ src/app/pages/assests/assests.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { IAsset } from 'src/app/modules/shared/interfaces/IAssests';
+import { Router } from '@angular/router';
+import { IProduct } from 'src/app/modules/shared/interfaces/IProduct';
 import { AuthService } from 'src/app/modules/shared/services/auth/auth.service';
 import { FirestoreService } from 'src/app/modules/shared/services/firestore/firestore.service';
 import { LoadingService } from 'src/app/modules/shared/services/loading/loading.service';
@@ -11,13 +12,14 @@ import { ToastService } from 'src/app/modules/shared/services/toast/toast.servic
   styleUrls: ['./assests.page.scss'],
 })
   export class AssestsPage implements OnInit {
-  protected assets!: IAsset[];
+  protected products!: IProduct[];
 
 
     constructor(private readonly _authSrv: AuthService,
       private readonly _firestoreSrv: FirestoreService,
       private readonly _loadingSrv: LoadingService,
-      private readonly _toastSrv: ToastService
+      private readonly _toastSrv: ToastService,
+      private readonly router: Router
     ) { }
 
     ngOnInit() {
@@ -27,9 +29,9 @@ import { ToastService } from 'src/app/modules/shared/services/toast/toast.servic
     public async loadProducts() {
       try {
         const userId = await this._authSrv.getAuthUserId();
-        this._firestoreSrv.getCollectionDocuments<IAsset>('products').subscribe(
+        this._firestoreSrv.getCollectionDocuments<IProduct>('products').subscribe(
           (products) => { 
-            this.assets = products;
+            this.products = products;
           },
           (error) => {
             console.error('Error loading products:', error);
@@ -39,17 +41,9 @@ import { ToastService } from 'src/app/modules/shared/services/toast/toast.servic
         console.error('error to load', error);
       }
     }
-
-    public async onLoanRequest(asset: IAsset){
-      if(!asset.state) {
-        try {
-          await this._firestoreSrv.updateProductState(asset.id, true);
-          this._toastSrv.showToast('Product has been marked as lent out.');
-        } catch (error) {
-          this._toastSrv.showToast('Failed to update product status.');
-        }
-      } else {
-        this._toastSrv.showToast('Product is already lent out.');
-      }
+    public async goToRequestPage(product: IProduct) {
+      console.log('Navigating with product:', product); 
+      await this.router.navigate(['/request'], { state: { product } }); 
     }
+
   }
